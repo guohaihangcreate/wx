e0f3dc3ad22e6bd240cc75460e9f3bec23cee5c5
guohaihangcreate <guohaihang@xiangmu.ren>
2018-07-11 18:16:14 +0800

    1

--- zpwx/src/com/create/core/WxAuthLogInServlet.java
+++ zpwx/src/com/create/core/WxAuthLogInServlet.java
@@ -3,20 +3,12 @@ package com.create.core;
 import java.io.IOException;
 import java.net.URLEncoder;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.net.URLCodec;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
-
+import com.create.po.UserInfo;
 import com.create.util.WxAuthUtil;
 
 import net.sf.json.JSONObject;
@@ -25,12 +17,12 @@ import net.sf.json.JSONObject;
 public class WxAuthLogInServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -2957653326431110490L;
-	
+	@Override
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
-		String callbackUrl = "http://t.cn/Rd4zj7V/callback";
+		String callbackUrl = "http://weixin.xiangmu.ren/zpwx/callback";
 		String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+WxAuthUtil.APPID
 				+ "&redirect_uri="+URLEncoder.encode(callbackUrl)
-				+ "I&response_type=code"
+				+ "&response_type=code"
 				+ "&scope=snsapi_userinfo"
 				+ "&state=STATE#wechat_redirect";
 		try {
---
+++ zpwx/src/com/create/core/servlet/CallBackServlet.java
@@ -0,0 +1,56 @@
+package com.create.core.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.create.po.UserInfo;
+import com.create.util.WeiXinUtil;
+import com.create.util.WxAuthUtil;
+
+import net.sf.json.JSONObject;
+@WebServlet("/callback")
+public class CallBackServlet extends HttpServlet {
+
+	public CallBackServlet() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		String code = request.getParameter("code");
+		String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+WxAuthUtil.APPID
+				+ "&secret="+WxAuthUtil.APPSECRET
+				+ "&code="+code
+				+ "&grant_type=authorization_code";		
+		JSONObject authObject = WxAuthUtil.doGetJson(url);
+		String token = authObject.getString("access_token");
+		String openid = authObject.getString("openid");
+//		String userurl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token="+token
+//				+ "&openid="+openid
+//				+ "&lang=zh_CN";
+		String userUrl = "https://api.weixin.qq.com/sns/userinfo?access_token="+token
+				+ "&openid="+openid
+				+ "&lang=zh_CN";
+		//获取用户信息
+		JSONObject userInfoObject = WxAuthUtil.doGetJson(userUrl);
+		UserInfo userinfo = new UserInfo();
+		userinfo.setNickname(userInfoObject.getString("nickname"));
+		userinfo.setCountry(userInfoObject.getString("country"));
+		userinfo.setCity(userInfoObject.getString("city"));
+		userinfo.setProvince(userInfoObject.getString("province"));
+		userinfo.setHeadimgurl(userInfoObject.getString("headimgurl"));
+		userinfo.setPrivilege(userInfoObject.getString("province"));
+		userinfo.setUnionid(userInfoObject.getString("privilege"));
+		userinfo.setSex(userInfoObject.getString("sex"));
+		request.setAttribute("userinfo", userinfo);
+//		String menueUrl= "https://api.weixin.qq.com/cgi-bin/menu/get?access_token="
+//				+ WeiXinUtil.getAccessToken().getToken();
+//				JSONObject menueObject =WeiXinUtil.doPostStr(menueUrl);
+		request.getRequestDispatcher("/myinfo.jsp").forward(request, response);
+	}
+
+}
